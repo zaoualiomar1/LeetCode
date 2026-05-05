4c842eed943794248332f3ff24618539954e2da3
Omar <omarzaouali98@gmail.com>
2026-05-05 10:09:32 +0200

    Middle of a linked list

---
+++ LinkedLists/876. Middle of the Linked List.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        fast = slow = head 
+        while fast and fast.next : 
+            fast = fast.next.next 
+            slow = slow.next 
+            
+        return slow 
+            
+            
